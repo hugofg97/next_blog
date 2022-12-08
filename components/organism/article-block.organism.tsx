2187ebbc82c506d3bdb05153a9300e63f5eb0769
hugofg97 <90161950+hugofg97@users.noreply.github.com>
2022-12-08 16:13:20 -0300

    bezerra

--- components/organism/article-block.organism.tsx
+++ components/organism/article-block.organism.tsx
@@ -27,53 +27,54 @@ export function OrganismArticleBlock(
   props: ArticleBlockOrganismInterface
 ): JSX.Element {
   return (
-    <article
-      className={`${styles.blogArticleBlock} ${props.classes.block}`}
-    >
-      <div className={`${styles.blogFeaturedImageBlock} ${props.classes.img}`}>
-        <Image
-          alt="helo"
-          sizes="
-          (max-width: 320px) 100vw,
-          (max-width: 1140px) 400px,
-          (max-width: 1240px) 400px,
-          (max-width: 1640px) 400px,
-          (max-width: 1740px) 400px,
-          (min-width: 1940px) 400px,
-          "
-          loading="lazy"
-          src={"/images/post.webp"}
-          fill
+    <></>
+    // <article
+    //   className={`${styles.blogArticleBlock} ${props.classes.block}`}
+    // >
+    //   <div className={`${styles.blogFeaturedImageBlock} ${props.classes.img}`}>
+    //     <Image
+    //       alt="helo"
+    //       sizes="
+    //       (max-width: 320px) 100vw,
+    //       (max-width: 1140px) 400px,
+    //       (max-width: 1240px) 400px,
+    //       (max-width: 1640px) 400px,
+    //       (max-width: 1740px) 400px,
+    //       (min-width: 1940px) 400px,
+    //       "
+    //       loading="lazy"
+    //       src={"/images/post.webp"}
+    //       fill
 
-          quality={10}
-        ></Image>
+    //       quality={10}
+    //     ></Image>
 
-        <div className={styles.blogArticleCategoryBadge}>
-          {props.categoryName}
-        </div>
-      </div>
-      <div className={`${styles.blogArticleContent} `}>
-        <Link href={`/${props.postSlug}`}>
-          <h1 style={{ textTransform: "none" }}>{props.title}</h1>
+    //     <div className={styles.blogArticleCategoryBadge}>
+    //       {props.categoryName}
+    //     </div>
+    //   </div>
+    //   <div className={`${styles.blogArticleContent} `}>
+    //     <Link href={`/${props.postSlug}`}>
+    //       <h1 style={{ textTransform: "none" }}>{props.title}</h1>
 
-          <p
-            className={styles.blogArticleExcerpt}
-            style={{ textTransform: "none" }}
-          >
-            {props.excerpt}
-          </p>
-        </Link>
-        <div className={styles.blogArticleMeta}>
-          <span style={{ color: "white" }}>{props.author}</span>
-          <span style={{ color: "white" }}>{props.date}</span>
-        </div>
-        <div className={styles.blogArticleContainerButton}>
-          <p style={{ color: "white", fontSize: "12px" }}> 0 Comentários</p>
-          <Link href={`/${props.postSlug}`}>
-            <button>Continue lendo</button>
-          </Link>
-        </div>
-      </div>
-    </article >
+    //       <p
+    //         className={styles.blogArticleExcerpt}
+    //         style={{ textTransform: "none" }}
+    //       >
+    //         {props.excerpt}
+    //       </p>
+    //     </Link>
+    //     <div className={styles.blogArticleMeta}>
+    //       <span style={{ color: "white" }}>{props.author}</span>
+    //       <span style={{ color: "white" }}>{props.date}</span>
+    //     </div>
+    //     <div className={styles.blogArticleContainerButton}>
+    //       <p style={{ color: "white", fontSize: "12px" }}> 0 Comentários</p>
+    //       <Link href={`/${props.postSlug}`}>
+    //         <button>Continue lendo</button>
+    //       </Link>
+    //     </div>
+    //   </div>
+    // </article >
   );
 }
