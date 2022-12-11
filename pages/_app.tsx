c82ac0c322c13c2664a27266d62b1dc862bd7314
hugofg97 <90161950+hugofg97@users.noreply.github.com>
2022-12-11 17:28:08 -0300

    .

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,4 @@
 import '../styles/globals.css'
-
-// import '../styles/organism/home/home-banner.scss'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
