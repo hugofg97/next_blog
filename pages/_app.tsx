bd2bfc44e7398585148d7690356aa3914fd5f3a5
hugofg97 <90161950+hugofg97@users.noreply.github.com>
2022-12-12 12:28:59 -0300

    ,

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,23 @@
 import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
+import { Poppins } from '@next/font/google'
 
+const poppins = Poppins({
+  weight: ['400', '700'],
+  style: ['normal', 'italic'],
+  subsets: ['latin'],
+})
 export default function App({ Component, pageProps }: AppProps) {
-  console.log('....')
-  return <Component {...pageProps} />
+
+  return (
+
+    <>
+      <style jsx global>{`
+      html, body {
+      font-family: ${poppins.style.fontFamily};
+    }
+  `}</style>
+      <Component {...pageProps} />
+    </>
+  )
 }
